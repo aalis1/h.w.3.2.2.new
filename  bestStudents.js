a2fdb150a1ac7c0bb97203ca8fdb12ad99e47421
aalis1 <aalisva1@gmail.com>
2024-01-21 15:05:29 +0000

    correction test

---  bestStudents.js
+++  bestStudents.js
@@ -8,34 +8,11 @@ students.forEach((students) => {
       bestStudents.push(student);
     }
     bestStudents.sort((a, b) => {
-      return b.score - a.score;
+      return a.score - b.score;
     });
   });
 });
 
 console.log(bestStudents);
-
-let result = bestStudents;
-
-function findWinner(bestStudents) {
-  let score = bestStudents.score;
-  let date = bestStudents.date;
-
-  if (bestStudents[0].score === Math.max(score)) {
-    console.log(`congratulations ${bestStudents[0].name}, you are the winner`);
-    return bestStudents[0].name;
-  } else bestStudents[0].score === bestStudents[1].score;
-  if (bestStudents[0].date < bestStudents[1].date) {
-    console.log(`congratulations ${bestStudents[0].name}, you are the winner`);
-  } else if (bestStudents[0].date > bestStudents[1].date) {
-    console.log(`congratulations ${bestStudents[1].name}, you are the winner`);
-  } else {
-    console.log(
-      `congratulations ${bestStudents[0].name}, ${bestStudents[1].name}, you are the winners`
-    );
-  }
-}
-
-findWinner(bestStudents);
 
 module.export = bestStudents;
--- bestStudents.test.js
+++ bestStudents.test.js
@@ -1,168 +1,187 @@
 const bestStudents = require(`./ bestStudents`);
+const students = require("./student-data.json");
 
-// describe("sort the student", () => {
-//   test("find students with positive scores", () => {
-//     expect(
-//       bestStudents.name && bestStudents.score !== 0 && bestStudents.date
-//     ).toEqual(
-//       bestStudents[
-//         ({ name: "Ivan", score: 35, date: "2022-10-11" },
-//         { name: "Stepan", score: 35, date: "2022-10-12" },
-//         { name: "Natalia", score: 25, date: "2022-10-10" },
-//         { name: "Marina", score: 25, date: "2022-10-01" },
-//         { name: "Dmitry", score: 25, date: "2022-10-12" },
-//         { name: "Zanna", score: 15, date: "2022-10-11" },
-//         { name: "Oleg", score: 9, date: "2022-10-01" },
-//         { name: "Maria", score: 5, date: "2022-10-10" },
-//         { name: "Vladimyr", score: 1, date: "2022-10-11" })
-//       ]
-//     );
-//   });
+describe("analise students[0]", () => {
+  test("find students with max scores", () => {
+    expect(
+      bestStudents.name && bestStudents.score === Math.max && bestStudents.date
+    ).toEqual(
+      students[
+        ({ name: "Ivan", score: 35, date: "2022-10-11" },
+        { name: "Stepan", score: 35, date: "2022-10-12" })
+      ]
+    );
+  });
 
-//   test("find the student who passed the test first", () => {
-//     expect(
-//       bestStudents.name &&
-//         bestStudents.score !== 0 &&
-//         bestStudents.date === Math.floor
-//     ).toEqual(
-//       bestStudents[
-//         ({ name: "Oleg", score: 9, date: "2022-10-01" },
-//         { name: "Marina", score: 25, date: "2022-10-01" })
-//       ]
-//     );
-//   });
+  test("find students with min scores", () => {
+    expect(
+      bestStudents.name && bestStudents.score === Math.min && bestStudents.date
+    ).toEqual(students[{ name: "Vladimyr", score: 1, date: "2022-10-11" }]);
+  });
 
-//   test("find students with max scores", () => {
-//     expect(
-//       bestStudents.name && bestStudents.score === Math.max && bestStudents.date
-//     ).toEqual(
-//       bestStudents[
-//         ({ name: "Ivan", score: 35, date: "2022-10-11" },
-//         { name: "Stepan", score: 35, date: "2022-10-12" })
-//       ]
-//     );
-//   });
+  test("find the student who passed the test first", () => {
+    expect(
+      bestStudents.name &&
+        bestStudents.score !== 0 &&
+        bestStudents.date === Math.floor
+    ).toEqual(bestStudents[{ name: "Oleg", score: 9, date: "2022-10-01" }]);
+  });
 
-//   test("find students with min positiv scores", () => {
-//     expect(
-//       bestStudents.name &&
-//         bestStudents.score === Math.floor &&
-//         bestStudents.date
-//     ).toEqual(bestStudents[{ name: "Vladimyr", score: 1, date: "2022-10-11" }]);
-//   });
+  test("find the best students with max scores", () => {
+    expect(
+      bestStudents.name &&
+        bestStudents.score === Math.max &&
+        bestStudents.date === Math.floor
+    ).toEqual(students[{ name: "Ivan", score: 35, date: "2022-10-11" }]);
+  });
+});
 
-//   test("find students with 0 scores", () => {
-//     expect(
-//       bestStudents.name && bestStudents.score === 0 && bestStudents.date
-//     ).toEqual(
-//       bestStudents[
-//         [
-//           { name: "Irina", score: 0, date: "2022-10-11" },
-//           { name: "Vasily", score: 0, date: "2022-10-10" },
-//           { name: "David", score: 0, date: "2022-10-11" },
-//           { name: "Kristina", score: 0, date: "2022-10-12" },
-//           { name: "Varvara", score: 0, date: "2022-10-01" },
-//           { name: "Tanya", score: 0, date: "2022-10-11" },
-//           { name: "Olga", score: 0, date: "" },
-//           { name: "Denis", score: 0, date: "2022-10-02" },
-//           { name: "Margo", score: 0, date: "2022-10-11" },
-//         ]
-//       ]
-//     );
-//   });
-// });
+describe("analise students[1]", () => {
+  test("find students with max scores", () => {
+    expect(
+      bestStudents.name && bestStudents.score === Math.max && bestStudents.date
+    ).toEqual(
+      students[
+        ({ name: "Natalia", score: 25, date: "2022-10-10" },
+        { name: "Marina", score: 25, date: "2022-10-01" },
+        { name: "Dmitry", score: 25, date: "2022-10-12" })
+      ]
+    );
+  });
 
-// describe("compare dates for students with the same max scores", () => {
-//   test("if the dates are the same", () => {
-//     expect(
-//       bestStudents.name &&
-//         bestStudents.score === Math.max &&
-//         bestStudents.date === equals
-//     ).toEqual(
-//       bestStudents[
-//         ({ name: "Ivan", score: 35, date: "2022-10-11" },
-//         { name: "Stepan", score: 35, date: "2022-10-12" })
-//       ]
-//     );
-//   });
-//   test("if the date of the first student is earlier than the date of the second", () => {
-//     expect(
-//       bestStudents.name &&
-//         bestStudents.score === Math.max &&
-//         bestStudents[0].date < bestStudents[1].date
-//     ).toEqual(bestStudents[{ name: "Ivan", score: 35, date: "2022-10-11" }]);
-//   });
-//   test("if the date of the second student is earlier than the date of the first", () => {
-//     expect(
-//       bestStudents.name &&
-//         bestStudents.score === Math.max &&
-//         bestStudents[0].date > bestStudents[1].date
-//     ).toEqual(bestStudents[{ name: "Stepan", score: 35, date: "2022-10-12" }]);
-//   });
-// });
+  test("find students with min scores", () => {
+    expect(
+      bestStudents.name && bestStudents.score === Math.min && bestStudents.date
+    ).toEqual(
+      students[
+        ({ name: "Margo", score: 0, date: "2022-10-11" },
+        { name: "Denis", score: 0, date: "2022-10-02" })
+      ]
+    );
+  });
+
+  test("find the student who passed the test first", () => {
+    expect(
+      bestStudents.name &&
+        bestStudents.score !== 0 &&
+        bestStudents.date === Math.floor
+    ).toEqual(bestStudents[{ name: "Marina", score: 25, date: "2022-10-01" }]);
+  });
+
+  test("find the best students with max scores", () => {
+    expect(
+      bestStudents.name &&
+        bestStudents.score === Math.max &&
+        bestStudents.date === Math.floor
+    ).toEqual(students[{ name: "Marina", score: 25, date: "2022-10-01" }]);
+  });
+});
+
+describe("analise students[2]", () => {
+  test("find students with max scores", () => {
+    expect(
+      bestStudents.name && bestStudents.score === Math.max && bestStudents.date
+    ).toBeUndefined();
+  });
+
+  test("find students with min scores", () => {
+    expect(
+      bestStudents.name && bestStudents.score === Math.min && bestStudents.date
+    ).toEqual(
+      students[
+        ({ name: "Irina", score: 0, date: "2022-10-11" },
+        { name: "Vasily", score: 0, date: "2022-10-10" },
+        { name: "David", score: 0, date: "2022-10-11" },
+        { name: "Kristina", score: 0, date: "2022-10-12" },
+        { name: "Varvara", score: 0, date: "2022-10-01" },
+        { name: "Tanya", score: 0, date: "2022-10-11" })
+      ]
+    );
+  });
+
+  test("find the student who passed the test first", () => {
+    expect(
+      bestStudents.name &&
+        bestStudents.score !== 0 &&
+        bestStudents.date === Math.floor
+    ).toBeUndefined();
+  });
+
+  test("find the best students with max scores", () => {
+    expect(
+      bestStudents.name &&
+        bestStudents.score === Math.max &&
+        bestStudents.date === Math.floor
+    ).toBeUndefined();
+  });
+});
+
+describe("analise bestStudents", () => {
+  test("find students with max scores", () => {
+    expect(
+      bestStudents.name && bestStudents.score === Math.max && bestStudents.date
+    ).toEqual(
+      students[
+        ({ name: "Ivan", score: 35, date: "2022-10-11" },
+        { name: "Stepan", score: 35, date: "2022-10-12" })
+      ]
+    );
+  });
+
+  test("find students with min scores", () => {
+    expect(
+      bestStudents.name && bestStudents.score === Math.min && bestStudents.date
+    ).toEqual(students[{ name: "Vladimyr", score: 1, date: "2022-10-11" }]);
+  });
+
+  test("find the student who passed the test first", () => {
+    expect(
+      bestStudents.name &&
+        bestStudents.score !== 0 &&
+        bestStudents.date === Math.floor
+    ).toEqual(
+      bestStudents[
+        ({ name: "Oleg", score: 9, date: "2022-10-01" },
+        { name: "Marina", score: 25, date: "2022-10-01" })
+      ]
+    );
+  });
+
+  test("find the best students with max scores", () => {
+    expect(
+      bestStudents.name &&
+        bestStudents.score === Math.max &&
+        bestStudents.date === Math.floor
+    ).toEqual(students[{ name: "Ivan", score: 35, date: "2022-10-11" }]);
+  });
+});
 
 //parametrized tests
-describe("sort the student", () => {
+
+describe("analise students[0]", () => {
   it.each([
     [
-      bestStudents.name && bestStudents.score !== 0 && bestStudents.date,
+      bestStudents.name && bestStudents.score === Math.max && bestStudents.date,
       [
-        bestStudents[
-          ({ name: "Ivan", score: 35, date: "2022-10-11" },
-          { name: "Stepan", score: 35, date: "2022-10-12" },
-          { name: "Natalia", score: 25, date: "2022-10-10" },
-          { name: "Marina", score: 25, date: "2022-10-01" },
-          { name: "Dmitry", score: 25, date: "2022-10-12" },
-          { name: "Zanna", score: 15, date: "2022-10-11" },
-          { name: "Oleg", score: 9, date: "2022-10-01" },
-          { name: "Maria", score: 5, date: "2022-10-10" },
-          { name: "Vladimyr", score: 1, date: "2022-10-11" })
-        ],
+        ({ name: "Ivan", score: 35, date: "2022-10-11" },
+        { name: "Stepan", score: 35, date: "2022-10-12" }),
       ],
+    ],
+    [
+      bestStudents.name && bestStudents.score === Math.min && bestStudents.date,
+      [{ name: "Vladimyr", score: 1, date: "2022-10-11" }],
     ],
     [
       bestStudents.name &&
         bestStudents.score !== 0 &&
         bestStudents.date === Math.floor,
-      [
-        bestStudents[
-          ({ name: "Oleg", score: 9, date: "2022-10-01" },
-          { name: "Marina", score: 25, date: "2022-10-01" })
-        ],
-      ],
-    ],
-    [
-      bestStudents.name && bestStudents.score === Math.max && bestStudents.date,
-      [
-        bestStudents[
-          ({ name: "Ivan", score: 35, date: "2022-10-11" },
-          { name: "Stepan", score: 35, date: "2022-10-12" })
-        ],
-      ],
+      [{ name: "Oleg", score: 9, date: "2022-10-01" }],
     ],
     [
       bestStudents.name &&
-        bestStudents.score === Math.floor &&
-        bestStudents.date,
-      [bestStudents[{ name: "Vladimyr", score: 1, date: "2022-10-11" }]],
-    ],
-    [
-      bestStudents.name && bestStudents.score === 0 && bestStudents.date,
-      [
-        bestStudents[
-          [
-            { name: "Irina", score: 0, date: "2022-10-11" },
-            { name: "Vasily", score: 0, date: "2022-10-10" },
-            { name: "David", score: 0, date: "2022-10-11" },
-            { name: "Kristina", score: 0, date: "2022-10-12" },
-            { name: "Varvara", score: 0, date: "2022-10-01" },
-            { name: "Tanya", score: 0, date: "2022-10-11" },
-            { name: "Olga", score: 0, date: "" },
-            { name: "Denis", score: 0, date: "2022-10-02" },
-            { name: "Margo", score: 0, date: "2022-10-11" },
-          ]
-        ],
-      ],
+        bestStudents.score === Math.max &&
+        bestStudents.date === Math.floor,
+      [{ name: "Ivan", score: 35, date: "2022-10-11" }],
     ],
   ])("variable values %p obtained roots %p", () => {
     expect(
@@ -171,30 +190,102 @@ describe("sort the student", () => {
   });
 });
 
-describe("compare dates for students with the same max scores", () => {
+describe("analise students[1]", () => {
   it.each([
     [
-      bestStudents.name &&
-        bestStudents.score === Math.max &&
-        bestStudents.date === equals,
+      bestStudents.name && bestStudents.score === Math.max && bestStudents.date,
       [
-        bestStudents[
-          ({ name: "Ivan", score: 35, date: "2022-10-11" },
-          { name: "Stepan", score: 35, date: "2022-10-12" })
-        ],
+        ({ name: "Natalia", score: 25, date: "2022-10-10" },
+        { name: "Marina", score: 25, date: "2022-10-01" },
+        { name: "Dmitry", score: 25, date: "2022-10-12" }),
+      ],
+    ],
+    [
+      bestStudents.name && bestStudents.score === Math.min && bestStudents.date,
+      [
+        ({ name: "Margo", score: 0, date: "2022-10-11" },
+        { name: "Denis", score: 0, date: "2022-10-02" }),
       ],
     ],
     [
       bestStudents.name &&
-        bestStudents.score === Math.max &&
-        bestStudents[0].date < bestStudents[1].date,
-      [bestStudents[{ name: "Ivan", score: 35, date: "2022-10-11" }]],
+        bestStudents.score !== 0 &&
+        bestStudents.date === Math.floor,
+      [{ name: "Marina", score: 25, date: "2022-10-01" }],
     ],
     [
       bestStudents.name &&
         bestStudents.score === Math.max &&
-        bestStudents[0].date > bestStudents[1].date,
-      [bestStudents[{ name: "Stepan", score: 35, date: "2022-10-12" }]],
+        bestStudents.date === Math.floor,
+      [{ name: "Marina", score: 25, date: "2022-10-01" }],
+    ],
+  ])("variable values %p obtained roots %p", () => {
+    expect(
+      bestStudents.name && bestStudents.score && bestStudents.date
+    ).toEqual(bestStudents[{}]);
+  });
+});
+
+describe("analise students[2]", () => {
+  it.each([
+    [
+      bestStudents.name && bestStudents.score === Math.max && bestStudents.date,
+      "there aren't roots",
+    ],
+    [
+      bestStudents.name && bestStudents.score === Math.min && bestStudents.date,
+      [
+        ({ name: "Irina", score: 0, date: "2022-10-11" },
+        { name: "Vasily", score: 0, date: "2022-10-10" },
+        { name: "David", score: 0, date: "2022-10-11" },
+        { name: "Kristina", score: 0, date: "2022-10-12" },
+        { name: "Varvara", score: 0, date: "2022-10-01" },
+        { name: "Tanya", score: 0, date: "2022-10-11" }),
+      ],
+    ],
+    [
+      bestStudents.name &&
+        bestStudents.score !== 0 &&
+        bestStudents.date === Math.floor,
+      "there aren't roots",
+    ],
+    [
+      bestStudents.name &&
+        bestStudents.score === Math.max &&
+        bestStudents.date === Math.floor,
+      "there aren't roots",
+    ],
+  ])("variable values %p obtained roots %p", () => {
+    expect(
+      bestStudents.name && bestStudents.score && bestStudents.date
+    ).toEqual(bestStudents[{}]);
+  });
+});
+
+describe("analise bestStudents", () => {
+  it.each([
+    [
+      bestStudents.name && bestStudents.score === Math.max && bestStudents.date,
+      [
+        ({ name: "Ivan", score: 35, date: "2022-10-11" },
+        { name: "Stepan", score: 35, date: "2022-10-12" }),
+      ],
+    ],
+    [
+      bestStudents.name && bestStudents.score === Math.min && bestStudents.date,
+      [{ name: "Vladimyr", score: 1, date: "2022-10-11" }],
+    ],
+    [
+      bestStudents.name &&
+        bestStudents.score !== 0 &&
+        bestStudents.date === Math.floor,
+      [{ name: "Marina", score: 25, date: "2022-10-01" }],
+    ],
+    [
+      bestStudents.name &&
+        bestStudents.score === Math.max &&
+        bestStudents.date === Math.floor,
+      [{ name: "Ivan", score: 35, date: "2022-10-11" }],
     ],
   ])("variable values %p obtained roots %p", () => {
     expect(
